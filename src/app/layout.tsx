243003e148272825f8abdf689199e536d0fcf7d0
leeftk <40748420+leeftk@users.noreply.github.com>
2026-05-11 21:24:16 +0000

    Revert accidental X tracking insertion (wrong repo)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Rye } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -52,11 +51,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <Script id="x-conversion-tracking" strategy="afterInteractive">
-          {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','rcd0e');`}
-        </Script>
-      </head>
       <body
         className={`${styreneA.className} ${styreneA.variable} antialiased text-foreground`}
       >
